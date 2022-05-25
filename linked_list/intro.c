fa002efcf788e42e31238a4ea80ff07ffb3e669b
caglayurdasal <caglayurdasal@gmail.com>
2022-05-26 00:31:16 +0300

    Add comments

--- linked_list/intro.c
+++ linked_list/intro.c
@@ -7,6 +7,51 @@ struct Node
     struct Node *link;
 };
 
+/* Inserts a new node from the head of linked list */
+void push(struct Node **pointerToHeadPtr, int val)
+{
+    /* Initialize the new node to be added */
+    struct Node *aux = (struct Node *)malloc(sizeof(struct Node));
+    /* Assign value to new node */
+    aux->data = val;
+    /* Make new node next pointer as head pointer */
+    aux->link = *pointerToHeadPtr;
+    /* Make head pointer point to the new node data */
+    (*pointerToHeadPtr) = aux;
+}
+/* Inserts a new node from any position */
+void insert(struct Node **pointerToHeadPtr, int n, int val)
+{
+    struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+    new->data = val;  // new node to hold the val
+    new->link = NULL; // initially points to NULL
+
+    if (n == 1) // If data is inserted from the head
+    {
+        new->link = *pointerToHeadPtr;
+        *pointerToHeadPtr = new;
+    }
+    else
+    {
+        struct Node *tempHead = (struct Node *)malloc(sizeof(struct Node));
+        tempHead = *pointerToHeadPtr; // It will be used to track the n. index
+        for (int i = 0; i < n - 2; i++)
+        {
+            tempHead = tempHead->link; // Assign node ptr to ptr to the next node to traverse the list
+        }
+        if (tempHead->link == NULL)
+        {
+            new->link = NULL;
+            tempHead->link = new;
+        }
+        else
+        {
+            new->link = tempHead->link;
+            tempHead->link = new;
+        }
+    }
+}
+/* Prints the linked list */
 void displayLinkedList(struct Node *head)
 {
     struct Node *aux = head;
@@ -24,49 +69,6 @@ void displayLinkedList(struct Node *head)
         aux = aux->link;
     }
     printf("\n");
-}
-/* Inserts a new node from the head of linked list */
-void push(struct Node **pointerToHeadPtr, int val)
-{
-    /* Initialize the new node to be added */
-    struct Node *aux = (struct Node *)malloc(sizeof(struct Node));
-    /* Assign value to new node */
-    aux->data = val;
-    /* Make new node next pointer as head pointer */
-    aux->link = *pointerToHeadPtr;
-    /* Make head pointer point to the new node data */
-    (*pointerToHeadPtr) = aux;
-}
-void insert(struct Node **pointerToHeadPtr, int n, int val)
-{
-    struct Node *temp1 = (struct Node *)malloc(sizeof(struct Node));
-    temp1->data = val;  // new node to hold the val
-    temp1->link = NULL; // initially points to NULL
-
-    if (n == 1) // If data is inserted from the head
-    {
-        temp1->link = *pointerToHeadPtr;
-        *pointerToHeadPtr = temp1;
-    }
-    else
-    {
-        struct Node *temp2 = (struct Node *)malloc(sizeof(struct Node));
-        temp2 = *pointerToHeadPtr;
-        for (int i = 0; i < n - 2; i++)
-        {
-            temp2 = temp2->link; // Assign node ptr to ptr to the next node to traverse the list
-        }
-        if (temp2->link == NULL)
-        {
-            temp1->link = NULL;
-            temp2->link = temp1;
-        }
-        else
-        {
-            temp1->link = temp2->link;
-            temp2->link = temp1;
-        }
-    }
 }
 int main()
 {
