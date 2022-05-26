5c5c51894ba67fd9f8418b860bfb7af1b2ff7f56
caglayurdasal <caglayurdasal@gmail.com>
2022-05-26 18:22:45 +0300

    Add deleteNode() prototype

---
+++ linked_list/deleteNode.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+/* Deletes the node specified with index n */
+void deleteNode(struct node **head, int key)
+{
+}
