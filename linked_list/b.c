aad045eb475ca8e4b3895e6b9f3b3b2b6c7b62de
caglayurdasal <caglayurdasal@gmail.com>
2022-05-28 19:49:14 +0300

    Add practice code

---
+++ linked_list/b.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct node
+{
+    int year;
+    char bookname[30];
+    char author[30];
+    struct node *next;
+};
+struct node *insert_node(struct node *root, char *bookname, char *author, int year)
+{
+    // Sort according to string into the node struct.
+    // It's name is "name".
+    // You will sort ascending order.
+    struct node *new = malloc(sizeof(struct node));
+    strcpy(new->author, author);
+    strcpy(new->bookname, bookname);
+    new->year = year;
+
+    new->next = NULL;
+
+    struct node *temp = malloc(sizeof(struct node)); // ptr to move head ptr
+    temp = root;
+
+    // If node is added at the head, move next of head ptr to address of new node
+    if (root == NULL)
+    {
+        new->next = root;
+        root = new;
+        return root;
+    }
+
+    if (temp->next != NULL)
+    {
+        if (root->year > root->next->year)
+        {
+            temp = root;
+            root = root->next;
+            root->next = temp;
+            return root;
+        }
+        else
+        {
+            root = root->next;
+        }
+        new->next = temp->next; // Assign next of new node to address of node at n.index
+        temp->next = new;       // Assign next of previos node to address of new node
+    }
+    else
+    {
+        temp->next = new; // Assign next of previos node to address of new node
+        new->next = NULL;
+    }
+
+    // if (root == NULL)
+    // {
+    //     root = root->next;
+    // }
+    // else
+    // {
+    //     struct node *temp;
+    //     while (root != NULL)
+    //     {
+    //         if (root->year > root->next->year)
+    //         {
+    //             temp = root;
+    //             root = root->next;
+    //             root->next = temp;
+    //             return root;
+    //         }
+    //         else
+    //         {
+    //             root = root->next;
+    //         }
+    //     }
+    // }
+    return root;
+}
+
+struct node *read_file()
+{
+    struct node *temp = malloc(sizeof(struct node));
+    temp->next = NULL;
+    FILE *fp;
+    fp = fopen("source.txt", "r");
+    printf("reading\n");
+    char name[30], aut[30];
+    int y;
+    while (!feof(fp))
+    {
+        fscanf(fp, "%s %s %d", name, aut, &y);
+        // printf("%s %s %d\n", name, aut, y);
+        insert_node(temp, name, aut, y);
+    }
+    return temp;
+}
+void print_nodes(struct node *root)
+{
+    while (root != NULL)
+    {
+        printf("%s  %s  %d\n", root->bookname, root->author, root->year);
+        root = root->next;
+    }
+    // if (root == NULL)
+    // {
+    //     return;
+    // }
+}
+struct node *delete_node(struct node *root, char *bookname)
+{
+    struct node *prev = malloc(sizeof(struct node));
+    struct node *temp = malloc(sizeof(struct node));
+    temp = (root);
+    prev = (root);
+    if (strcmp(temp->bookname, bookname) == 0) // if head is to be deleted
+    {
+        root = temp->next;
+        return root;
+    }
+    while (temp != NULL)
+    {
+        temp = temp->next;                         // move temp ptr by 1 node
+        if (strcmp(temp->bookname, bookname) == 0) // if data pointed by temp ptr is to be deleted
+        {
+            if (temp->next == NULL) // if tail is to be deleted
+            {
+                prev->next = NULL;
+                return root;
+            }
+            prev->next = temp->next; // assign previous node to next of target node
+            return root;
+        }
+        prev = prev->next;
+    }
+    return root;
+}
+int main()
+{
+    struct node *head = malloc(sizeof(struct node));
+    head->next = NULL;
+    printf("Reading the source.txt file...\n");
+    head = read_file();
+    printf("Printing the linked list...\n\n");
+    print_nodes(head);
+
+    char deleted[30];
+    printf("Which book do you want to delete? ");
+    scanf("%s", deleted);
+
+    delete_node(head, deleted);
+    print_nodes(head);
+}
