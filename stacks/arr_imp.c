46db86e5b4ec7b02c410c1815561260ee66c6b51
Çağla <caglayurdasal@gmail.com>
2022-05-29 12:17:45 +0300

    Add pop(), getTop() and print()

--- stacks/arr_imp.c
+++ stacks/arr_imp.c
@@ -1,33 +1,53 @@
+/* Stack: Array based implementation */
+
 #include <stdio.h>
 
 #define MAX 101
 int arr[MAX];
-int top = -1;
+int top = -1; // initially stack is empty
 
-void push(int *arr, int n)
+void push(int n)
 {
-    arr[++top]; // preincrement op used to increment before asignment
+    if (top == MAX - 1)
+    {
+        printf("Error! Stack overflow\n");
+        return;
+    }
+    arr[++top] = n; // preincrement op is used to increment before asignment
 }
-
+void pop()
+{
+    if (top == -1)
+    {
+        printf("Error! Underflow\n");
+        return;
+    }
+    top--;
+}
+int getTop()
+{
+    return arr[top];
+}
+void print()
+{
+    for (int i = 0; i <= top; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
 int main()
 {
-    int num_nodes, new;
-    printf("Num nodes: ");
-    scanf("%d", &num_nodes);
 
-    for (int i = 0; i < num_nodes; i++)
-    {
-
-        printf("Node %d: ", i + 1);
-        scanf("%d", &new);
-        if (i != num_nodes - 1)
-        {
-            printf("%d -> ", arr[top]);
-        }
-        else
-        {
-            printf("%d\n", arr[top]);
-        }
-    }
+    push(5);
+    print();
+    push(88);
+    print();
+    push(999);
+    print();
+    pop();
+    print();
+    push(12);
+    print();
     return 0;
 }
