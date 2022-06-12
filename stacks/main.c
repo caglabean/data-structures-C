11715384ddd00f5bea0b63202b1fcf67053b76c8
caglayurdasal <caglayurdasal@gmail.com>
2022-06-12 16:36:46 +0300

    Add function to create stack

--- stacks/main.c
+++ stacks/main.c
@@ -1,4 +1,23 @@
-//
-// Created by Çağla on 12.06.2022.
-//
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct Stack
+{
+    int top;
+    int capacity;
+    int *arr;
+} Stack;
+
+Stack *createStack(int capacity)
+{
+    Stack *stack = (Stack *)malloc(sizeof(Stack)); // Allocate memo for the entire stack
+    stack->top = -1;
+    stack->capacity = capacity;
+    stack->arr = (Stack *)malloc(sizeof(Stack) * capacity); // Allocate memo for members of stack array
+
+    return stack;
+}
+
+int main()
+{
+}
