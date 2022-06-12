a12f8030a22b4905a250d967e203bb6bb929ff60
caglayurdasal <caglayurdasal@gmail.com>
2022-06-12 17:16:08 +0300

    Update driver code

--- stacks/main.c
+++ stacks/main.c
@@ -1,23 +1,24 @@
+#include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Stack
-{
-    int top;
-    int capacity;
-    int *arr;
-} Stack;
-
-Stack *createStack(int capacity)
-{
-    Stack *stack = (Stack *)malloc(sizeof(Stack)); // Allocate memo for the entire stack
-    stack->top = -1;
-    stack->capacity = capacity;
-    stack->arr = (Stack *)malloc(sizeof(Stack) * capacity); // Allocate memo for members of stack array
-
-    return stack;
-}
-
 int main()
 {
+    int popped;
+    Stack *stack = createStack(4);
+
+    if ((popped = pop(stack)) != 0)
+        printf("Popped element: %d\n", popped);
+    push(42, stack);
+    peek(stack);
+    push(56, stack);
+    push(33, stack);
+    peek(stack);
+    if ((popped = pop(stack)) != 0)
+        printf("Popped element: %d\n", popped);
+    peek(stack);
+    push(99, stack);
+    peek(stack);
+    push(588, stack);
+    push(23, stack);
 }
